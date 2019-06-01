e879c0d867e6bf61ad8c735520d3333879d4f7fb
ChinmaiVignyaKolli <51255436+ChinmaiVignyaKolli@users.noreply.github.com>
2019-06-01 10:42:13 +0530

    Update osm_parse.py

--- osm_parse.py
+++ osm_parse.py
@@ -1,3 +1,5 @@
+#Run the following commands on your terminal before executing the code :
+
 
 #sudo apt-get install osmctools
 #sudo apt-get update
@@ -8,7 +10,7 @@ import pandas as pd
 
 
 #osmconvert map.osm --all-to-nodes --csv="@id @lon @lat amenity shop name" --csv-headline -o=amenity.csv
-
+#This command return a tab seperated values file which can be converted to csv as follows.
 
 tsv_file='amenity.csv'
 csv_table=pd.read_csv('amenity.csv',sep='\t')
@@ -18,21 +20,26 @@ csv_table.to_csv('amenity1.csv',index=False)
 df = pandas.read_csv('amenity1.csv')
 a = df['name'].notnull()
 df1 =df[a]
-#print (df1[['name','amenity','shop']])
-
+#print (df1[['name','amenity','shop']])          #Prints shops in the area
 shops = df1['shop'].notnull()
 print(df1[shops])
 
 print('\n')
+
 amenities = df['amenity'].notnull()
 amen = df[amenities]
 print ("Hospitals in the area are :")
-print (df[df.amenity.str.contains("hospital", na=False)])
+print (df[df.amenity.str.contains("hospital", na=False)])    #Prints amenities in the area
+
+
 countx = df.amenity.str.contains("hospital", na=False)
 print('\n')
 print("No of hospitals in the area are :\n")
 trues = np.count_nonzero(countx)
 print (trues)
+
+
+
 #osmconvert shops.osm --all-to-nodes --csv="@id @lon @lat building name" --csv-headline -o = buildings.csv
 print('\n')
 tsv_file='buildings.csv'
@@ -40,7 +47,7 @@ csv_table=pd.read_csv('buildings.csv',sep='\t')
 csv_table.to_csv('buildings1.csv',index=False)
 
 dfb = pandas.read_csv('buildings1.csv')
-b = dfb['building'].notnull()
+b = dfb['building'].notnull()                         #Prints buildings in the area
 print(dfb[b])
 
 #osmconvert map.osm --all-to-nodes --csv="@id @lon @lat power name voltage" --csv-headline -o=power.csv
@@ -49,7 +56,7 @@ tsv_file='buildings.csv'
 csv_table=pd.read_csv('power.csv',sep='\t')
 csv_table.to_csv('power1.csv',index=False)
 
-dfp = pandas.read_csv('power1.csv')
+dfp = pandas.read_csv('power1.csv')                     #Prints power lines in the area
 p = dfp['power'].notnull()
 print(dfp[p])
 
@@ -63,7 +70,7 @@ csv_table.to_csv('landcover1.csv',index=False)
 lc = pandas.read_csv('landcover1.csv')
 l = lc['landuse'].notnull()
 lu = lc[l]
-print(lu[['@id','@lat','@lon','landuse']])
+print(lu[['@id','@lat','@lon','landuse']])                        #Prints land use in the area
 
 print('\n')
  
